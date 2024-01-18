8845483d2687e05486d8fa679dcc2ed045c3480b
mittal0802 <mittalkeshav41@gmail.com>
2024-01-18 12:16:26 +0530

    added acknowledgement, sending location and filtering bad words feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,7 +9,7 @@ const socket = io();
 //   socket.emit("increment");
 // });
 
-socket.on("welcomeMessage", (message) => {
+socket.on("serverMessage", (message) => {
   console.log(message);
 });
 
@@ -19,5 +19,29 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   // const message = document.querySelector("input").value;
   const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered!");
+  });
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location shared!");
+      }
+    );
+  });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,10 +16,18 @@ let count = 0;
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
-  socket.emit("welcomeMessage", "Welcome! You are connected to the chat app!");
+  socket.emit("serverMessage", "Welcome! You are connected to the chat app!");
 
-  socket.on("sendMessage", (message) => {
-    io.emit("welcomeMessage", message);
+  //this will send a message to everyone except the current connection
+  socket.broadcast.emit("serverMessage", "A new user has joined!");
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
+    io.emit("serverMessage", message);
+    callback();
   });
   // socket.emit("countUpdated", count);
 
@@ -27,6 +36,21 @@ io.on("connection", (socket) => {
   //   // socket.emit("countUpdated", count); // emit to a specific connection
   //   io.emit("countUpdated", count); // emit to all connections
   // });
+
+  //location
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "serverMessage",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+    callback();
+  });
+
+  //for disconnecting
+  socket.on("disconnect", () => {
+    //no need to broadcast because the connection is already closed
+    io.emit("serverMessage", "A user has left!");
+  });
 });
 
 server.listen(port, () => {
