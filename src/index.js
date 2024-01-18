52fd0cfdf810f5e7763dd009ee370fe3a734221f
mittal0802 <mittalkeshav41@gmail.com>
2024-01-18 11:36:21 +0530

    built a socket connection for simple chat feature

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
-const app = express();
+const socketio = require("socket.io");
 
+const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirPath));
 
-app.get("/", (req, res) => {
-  res.send("Hello express!");
+let count = 0;
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("welcomeMessage", "Welcome! You are connected to the chat app!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("welcomeMessage", message);
+  });
+  // socket.emit("countUpdated", count);
+
+  // socket.on("increment", () => {
+  //   count++;
+  //   // socket.emit("countUpdated", count); // emit to a specific connection
+  //   io.emit("countUpdated", count); // emit to all connections
+  // });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Server is up on port " + port);
 });
